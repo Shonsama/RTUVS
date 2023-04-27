ea7591c8fba168f52af8d3243bd938f619e2385e
Shonsama <36616241+Shonsama@users.noreply.github.com>
2023-04-27 06:06:59 +0000

    bug fix

--- web/components/Stream.tsx
+++ web/components/Stream.tsx
@@ -2,21 +2,15 @@ import { useEffect, useState } from 'react';
 type StreamProps = {
     topicName: String;
 };
-const Stream: React.FC<StreamProps> = ({ topicname }) => {
+const Stream: React.FC<StreamProps> = ({ topicName }) => {
   const [streamUrl, setStreamUrl] = useState('');
 
   useEffect(() => {
     const fetchStreamUrl = async () => {
-      try {
-        const response = await axios.get(`/api/stream?topicname=${topicname}`);
-        setStreamUrl(response.data.streamUrl);
-      } catch (error) {
-        console.error(error);
-      }
     };
 
     fetchStreamUrl();
-  }, [topicname]);
+  }, [topicName]);
 
   if (!streamUrl) {
     return <div>Loading...</div>;
